8b1dc3d6766a2fc900dc361b27627dc9906af533
intern062 <kenilpanchal111@gmail.com>
2025-03-25 15:08:53 +0530

    Create Auth repo with user

--- Repo/Interface/IAuthInterface.cs
+++ Repo/Interface/IAuthInterface.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Repo.Interfaces
+namespace Repo
 {
-    public interface IUserInterface
+    public interface IAuthInterface
     {
         public Task<bool> RegisterUser(User user);
         public Task<bool> IsEmailExists(string email);  
