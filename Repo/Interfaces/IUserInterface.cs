badafc88fe453f952c8b569b0c52fb889a58993c
intern062 <kenilpanchal111@gmail.com>
2025-03-25 14:49:29 +0530

    change the naming

--- Repo/Interfaces/IUserInterface.cs
+++ Repo/Interfaces/IUserInterface.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Repo.Interfaces
 {
-    public interface IUserRepository
+    public interface IUserInterface
     {
         public Task<bool> RegisterUser(User user);
         public Task<bool> IsEmailExists(string email);  
