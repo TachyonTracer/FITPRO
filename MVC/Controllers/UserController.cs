190d4de9db5a4c363f40d770af4b77a83b1ff78c
Solanki-jignesh <jigneshsolanki7077@gmail.com>
2025-04-01 14:19:35 +0530

    Add design of Bookclass, issue : instructor name is not present in class model

--- MVC/Controllers/UserController.cs
+++ MVC/Controllers/UserController.cs
@@ -5,18 +5,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Repo;
 
 namespace MVC
 {
     public class UserController : Controller
     {
 
+        private readonly IClassInterface _Class;
+        public UserController(IClassInterface Class)
+        {
+            _Class = Class;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult BookClass(){
+
+      [HttpGet]
+        public async Task<IActionResult> BookClass(string id)
+        {
+
+            Class ClassDetails = await _Class.GetOne("42");
+            if (ClassDetails != null)
+            {
+                Console.WriteLine(ClassDetails);
+
+                return View(ClassDetails);
+            }
             return View();
         }
 
