9c0deb3c4265d9668d6b25b029b413dcc40c0e72
Janshree12 <janshreesharma@gmail.com>
2025-04-11 15:06:07 +0530

    Dynamic navbar and authtoken added in user dashboard

--- MVC/wwwroot/js/Auth/Login.js
+++ MVC/wwwroot/js/Auth/Login.js
@@ -282,7 +282,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }).then(() => {
             // Perform redirection based on user role
             if (result.userRole == "user") {
-              window.location.href = "/user/Dashboard";  
+              window.location.href = "/user/index";  
             } else if (result.userRole == "instructor") {
               window.location.href = "/instructor";
             } else if (result.userRole == "admin") {
